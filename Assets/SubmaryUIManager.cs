df8c9bd5e5303fe99a7d023e3597305e3d61469c
Kolobit758 <kobchokbn@gmail.com>
2025-10-29 22:08:41 +0700

    New ver

---
+++ Assets/SubmaryUIManager.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+
+public class SubmaryUIManager : MonoBehaviour
+{
+
+    public List<Image> submaryImage = new List<Image>();
+    public List<string> submaryText = new List<string>();
+    
+}
